95a8099ab9edf9ac99eba68fda704d00f24dc0a1
Whitney Vo <whitney.vo@ufl.edu>
2021-04-08 22:34:56 -0400

    Added contact page images, started contact styles
    Need better solution for "summon" image. Still tweaking
    contact-betelgeuse.js

--- js/contact-betelgeuse.js
+++ js/contact-betelgeuse.js
@@ -23,12 +23,27 @@ callout.onclick = function () {
 // Bring up Betelgeuse image and play "showtime" audio on 3rd click
 var click_count = 0;
 
+let summoningSpot = document.getElementById("summoning-spot");
+
+let wallpaper = document.getElementById("wallpaper");
+
+function showTimeAudio() {
+  summoningSpot.innerHTML = "<audio autoplay autobuffer><source src='audio/showtime.ogg' type='audio/ogg'></source><source src='audio/showtime.mp3' type='audio/mpeg'></source></audio>";
+};
+
+function showTimeBG(){
+  wallpaper.style.backgroundImage = "url('images/betelgeuse-showtime.png')";
+};
+
 $("#clickable").on( "click", function() {
   click_count++;
 
     if(click_count == 3){
         click_count = 0;
-         setTimeout(function(){ document.getElementById("contact-form").innerHTML = "<figure class = 'betelgeuse-showtime'><img src = 'betelgeuse.jpg'></figure><audio autoplay autobuffer><source src='audio/showtime.ogg' type='audio/ogg'></source><source src='audio/showtime.mp3' type='audio/mpeg'></source></audio>";}, 1500);
+         setTimeout(showTimeAudio, 1500);
+         setTimeout(showTimeBG, 1500);
     }
 
 });
+
+// setTimeout(function(){ document.getElementById("summoning-spot").innerHTML = "<figure class = 'betelgeuse-showtime'><img src = 'images/beetlejuice-breakout.png'></figure><audio autoplay autobuffer><source src='audio/showtime.ogg' type='audio/ogg'></source><source src='audio/showtime.mp3' type='audio/mpeg'></source></audio>";}, 1500);
