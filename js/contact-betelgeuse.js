40a15a4f92a93318b1d0853e4c8cf4f17c6f14bb
Whitney Vo <whitney.vo@ufl.edu>
2021-04-09 20:46:23 -0700

    Contact page styling, javascript. We're getting there XD

--- js/contact-betelgeuse.js
+++ js/contact-betelgeuse.js
@@ -23,16 +23,29 @@ callout.onclick = function () {
 // Bring up Betelgeuse image and play "showtime" audio on 3rd click
 var click_count = 0;
 
-let summoningSpot = document.getElementById("summoning-spot");
-
+let summoningSpot = document.getElementById("summoningSpot");
+let showtime = document.getElementById("showTimeAudio");
+let mirror = document.getElementById("mirror");
 let wallpaper = document.getElementById("wallpaper");
 
 function showTimeAudio() {
-  summoningSpot.innerHTML = "<audio autoplay autobuffer><source src='audio/showtime.ogg' type='audio/ogg'></source><source src='audio/showtime.mp3' type='audio/mpeg'></source></audio>";
+  showtime.play();
 };
 
-function showTimeBG(){
-  wallpaper.style.backgroundImage = "url('images/betelgeuse-showtime.png')";
+function hideMirror() {
+  mirror.style.opacity = "0";
+};
+
+function hideSummoningSpot() {
+  summoningSpot.style.opacity = "0";
+}
+
+function showBG () {
+  wallpaper.style.background = "url('images/betelgeuse-showtime.png') no-repeat center";
+};
+
+function fullCover () {
+  wallpaper.style.backgroundSize = "cover";
 };
 
 $("#clickable").on( "click", function() {
@@ -41,7 +54,10 @@ $("#clickable").on( "click", function() {
     if(click_count == 3){
         click_count = 0;
          setTimeout(showTimeAudio, 1500);
-         setTimeout(showTimeBG, 1500);
+         setTimeout(hideMirror, 1500);
+         setTimeout(hideSummoningSpot, 1500);
+         setTimeout(showBG, 2000);
+         setTimeout(fullCover, 2000);
     }
 
 });
